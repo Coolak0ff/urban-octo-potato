d7af40038200ea7d8e9a293d6fcf6f351eb3e84f
Constantine <kostr007@gmail.com>
2023-02-28 17:13:45 +0300

    new

---
+++ master.vb
@@ -0,0 +1,245 @@
+Sub Ìàêðîñ8()
+'
+' Ìàêðîñ8 Ìàêðîñ
+'
+
+'
+    Sheets.Add.Name = "MasterSheet"
+    Range("B2").Select
+    ActiveCell.FormulaR1C1 = "1/1/2023"
+    Range("B3").Select
+    ActiveCell.FormulaR1C1 = "1/2/2023"
+    Range("B4").Select
+    ActiveCell.FormulaR1C1 = "1/3/2023"
+    Range("B5").Select
+    ActiveCell.FormulaR1C1 = "1/4/2023"
+    Range("B6").Select
+    ActiveCell.FormulaR1C1 = "1/5/2023"
+    Range("B7").Select
+    ActiveCell.FormulaR1C1 = "1/6/2023"
+    Range("B8").Select
+    ActiveCell.FormulaR1C1 = "1/7/2023"
+    Range("B9").Select
+    ActiveCell.FormulaR1C1 = "1/8/2023"
+    Range("B10").Select
+    ActiveCell.FormulaR1C1 = "1/14/2023"
+    Range("B11").Select
+    ActiveCell.FormulaR1C1 = "1/15/2023"
+    Range("B12").Select
+    ActiveCell.FormulaR1C1 = "1/21/2023"
+    Range("B13").Select
+    ActiveCell.FormulaR1C1 = "1/22/2023"
+    Range("B14").Select
+    ActiveCell.FormulaR1C1 = "1/28/2023"
+    Range("B15").Select
+    ActiveCell.FormulaR1C1 = "1/29/2023"
+    Range("B16").Select
+    ActiveCell.FormulaR1C1 = "2/4/2023"
+    Range("B17").Select
+    ActiveCell.FormulaR1C1 = "2/5/2023"
+    Range("B18").Select
+    ActiveCell.FormulaR1C1 = "2/11/2023"
+    Range("B19").Select
+    ActiveCell.FormulaR1C1 = "2/12/2023"
+    Range("B20").Select
+    ActiveCell.FormulaR1C1 = "2/18/2023"
+    Range("B21").Select
+    ActiveCell.FormulaR1C1 = "2/19/2023"
+    Range("B22").Select
+    ActiveCell.FormulaR1C1 = "2/23/2023"
+    Range("B23").Select
+    ActiveCell.FormulaR1C1 = "2/24/2023"
+    Range("B24").Select
+    ActiveCell.FormulaR1C1 = "2/25/2023"
+    Range("B25").Select
+    ActiveCell.FormulaR1C1 = "2/26/2023"
+    Range("B26").Select
+    ActiveCell.FormulaR1C1 = "3/4/2023"
+    Range("B27").Select
+    ActiveCell.FormulaR1C1 = "3/5/2023"
+    Range("B28").Select
+    ActiveCell.FormulaR1C1 = "3/8/2023"
+    Range("B29").Select
+    ActiveCell.FormulaR1C1 = "3/11/2023"
+    Range("B30").Select
+    ActiveCell.FormulaR1C1 = "3/12/2023"
+    Range("B31").Select
+    ActiveCell.FormulaR1C1 = "3/18/2023"
+    Range("B32").Select
+    ActiveCell.FormulaR1C1 = "3/19/2023"
+    Range("B33").Select
+    ActiveCell.FormulaR1C1 = "3/25/2023"
+    Range("B34").Select
+    ActiveCell.FormulaR1C1 = "3/26/2023"
+    Range("B35").Select
+    ActiveCell.FormulaR1C1 = "4/1/2023"
+    Range("B36").Select
+    ActiveCell.FormulaR1C1 = "4/2/2023"
+    Range("B37").Select
+    ActiveCell.FormulaR1C1 = "4/8/2023"
+    Range("B38").Select
+    ActiveCell.FormulaR1C1 = "4/9/2023"
+    Range("B39").Select
+    ActiveCell.FormulaR1C1 = "4/15/2023"
+    Range("B40").Select
+    ActiveCell.FormulaR1C1 = "4/16/2023"
+    Range("B41").Select
+    ActiveCell.FormulaR1C1 = "4/22/2023"
+    Range("B42").Select
+    ActiveCell.FormulaR1C1 = "4/23/2023"
+    Range("B43").Select
+    ActiveCell.FormulaR1C1 = "4/29/2023"
+    Range("B44").Select
+    ActiveCell.FormulaR1C1 = "4/30/2023"
+    Range("B45").Select
+    ActiveCell.FormulaR1C1 = "5/1/2023"
+    Range("B46").Select
+    ActiveCell.FormulaR1C1 = "5/6/2023"
+    Range("B47").Select
+    ActiveCell.FormulaR1C1 = "5/7/2023"
+    Range("B48").Select
+    ActiveCell.FormulaR1C1 = "5/8/2023"
+    Range("B49").Select
+    ActiveCell.FormulaR1C1 = "5/9/2023"
+    Range("B50").Select
+    ActiveCell.FormulaR1C1 = "5/13/2023"
+    Range("B51").Select
+    ActiveCell.FormulaR1C1 = "5/14/2023"
+    Range("B52").Select
+    ActiveCell.FormulaR1C1 = "5/20/2023"
+    Range("B53").Select
+    ActiveCell.FormulaR1C1 = "5/21/2023"
+    Range("B54").Select
+    ActiveCell.FormulaR1C1 = "5/27/2023"
+    Range("B55").Select
+    ActiveCell.FormulaR1C1 = "5/28/2023"
+    Range("B56").Select
+    ActiveCell.FormulaR1C1 = "6/3/2023"
+    Range("B57").Select
+    ActiveCell.FormulaR1C1 = "6/4/2023"
+    Range("B58").Select
+    ActiveCell.FormulaR1C1 = "6/10/2023"
+    Range("B59").Select
+    ActiveCell.FormulaR1C1 = "6/11/2023"
+    Range("B60").Select
+    ActiveCell.FormulaR1C1 = "6/12/2023"
+    Range("B61").Select
+    ActiveCell.FormulaR1C1 = "6/17/2023"
+    Range("B62").Select
+    ActiveCell.FormulaR1C1 = "6/18/2023"
+    Range("B63").Select
+    ActiveCell.FormulaR1C1 = "6/24/2023"
+    Range("B64").Select
+    ActiveCell.FormulaR1C1 = "6/25/2023"
+    Range("B65").Select
+    ActiveCell.FormulaR1C1 = "7/1/2023"
+    Range("B66").Select
+    ActiveCell.FormulaR1C1 = "7/2/2023"
+    Range("B67").Select
+    ActiveCell.FormulaR1C1 = "7/8/2023"
+    Range("B68").Select
+    ActiveCell.FormulaR1C1 = "7/9/2023"
+    Range("B69").Select
+    ActiveCell.FormulaR1C1 = "7/15/2023"
+    Range("B70").Select
+    ActiveCell.FormulaR1C1 = "7/16/2023"
+    Range("B71").Select
+    ActiveCell.FormulaR1C1 = "7/22/2023"
+    Range("B72").Select
+    ActiveCell.FormulaR1C1 = "7/23/2023"
+    Range("B73").Select
+    ActiveCell.FormulaR1C1 = "7/29/2023"
+    Range("B74").Select
+    ActiveCell.FormulaR1C1 = "7/30/2023"
+    Range("B75").Select
+    ActiveCell.FormulaR1C1 = "8/5/2023"
+    Range("B76").Select
+    ActiveCell.FormulaR1C1 = "8/6/2023"
+    Range("B77").Select
+    ActiveCell.FormulaR1C1 = "8/12/2023"
+    Range("B78").Select
+    ActiveCell.FormulaR1C1 = "8/13/2023"
+    Range("B79").Select
+    ActiveCell.FormulaR1C1 = "8/19/2023"
+    Range("B80").Select
+    ActiveCell.FormulaR1C1 = "8/20/2023"
+    Range("B81").Select
+    ActiveCell.FormulaR1C1 = "8/26/2023"
+    Range("B82").Select
+    ActiveCell.FormulaR1C1 = "8/27/2023"
+    Range("B83").Select
+    ActiveCell.FormulaR1C1 = "9/2/2023"
+    Range("B84").Select
+    ActiveCell.FormulaR1C1 = "9/3/2023"
+    Range("B85").Select
+    ActiveCell.FormulaR1C1 = "9/9/2023"
+    Range("B86").Select
+    ActiveCell.FormulaR1C1 = "9/10/2023"
+    Range("B87").Select
+    ActiveCell.FormulaR1C1 = "9/16/2023"
+    Range("B88").Select
+    ActiveCell.FormulaR1C1 = "9/17/2023"
+    Range("B89").Select
+    ActiveCell.FormulaR1C1 = "9/23/2023"
+    Range("B90").Select
+    ActiveCell.FormulaR1C1 = "9/24/2023"
+    Range("B91").Select
+    ActiveCell.FormulaR1C1 = "9/30/2023"
+    Range("B92").Select
+    ActiveCell.FormulaR1C1 = "10/1/2023"
+    Range("B93").Select
+    ActiveCell.FormulaR1C1 = "10/7/2023"
+    Range("B94").Select
+    ActiveCell.FormulaR1C1 = "10/8/2023"
+    Range("B95").Select
+    ActiveCell.FormulaR1C1 = "10/14/2023"
+    Range("B96").Select
+    ActiveCell.FormulaR1C1 = "10/15/2023"
+    Range("B97").Select
+    ActiveCell.FormulaR1C1 = "10/21/2023"
+    Range("B98").Select
+    ActiveCell.FormulaR1C1 = "10/22/2023"
+    Range("B99").Select
+    ActiveCell.FormulaR1C1 = "10/28/2023"
+    Range("B100").Select
+    ActiveCell.FormulaR1C1 = "10/29/2023"
+    Range("B101").Select
+    ActiveCell.FormulaR1C1 = "11/4/2023"
+    Range("B102").Select
+    ActiveCell.FormulaR1C1 = "11/5/2023"
+    Range("B103").Select
+    ActiveCell.FormulaR1C1 = "11/6/2023"
+    Range("B104").Select
+    ActiveCell.FormulaR1C1 = "11/11/2023"
+    Range("B105").Select
+    ActiveCell.FormulaR1C1 = "11/12/2023"
+    Range("B106").Select
+    ActiveCell.FormulaR1C1 = "11/18/2023"
+    Range("B107").Select
+    ActiveCell.FormulaR1C1 = "11/19/2023"
+    Range("B108").Select
+    ActiveCell.FormulaR1C1 = "11/25/2023"
+    Range("B109").Select
+    ActiveCell.FormulaR1C1 = "11/26/2023"
+    Range("B110").Select
+    ActiveCell.FormulaR1C1 = "12/2/2023"
+    Range("B111").Select
+    ActiveCell.FormulaR1C1 = "12/3/2023"
+    Range("B112").Select
+    ActiveCell.FormulaR1C1 = "12/9/2023"
+    Range("B113").Select
+    ActiveCell.FormulaR1C1 = "12/10/2023"
+    Range("B114").Select
+    ActiveCell.FormulaR1C1 = "12/16/2023"
+    Range("B115").Select
+    ActiveCell.FormulaR1C1 = "12/17/2023"
+    Range("B116").Select
+    ActiveCell.FormulaR1C1 = "12/23/2023"
+    Range("B117").Select
+    ActiveCell.FormulaR1C1 = "12/24/2023"
+    Range("B118").Select
+    ActiveCell.FormulaR1C1 = "12/30/2023"
+    Range("B119").Select
+    ActiveCell.FormulaR1C1 = "12/31/2023"
+
+End Sub
